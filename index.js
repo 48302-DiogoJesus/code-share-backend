7b422b57540efcf0904e1b5a57f789b6a28ccb41
unknown <a48302@alunos.isel.pt>
2021-10-15 22:42:23 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors : {
+        origin: '*'
+    }
+});
+
+const SERVER_PORT = process.env.PORT || 3000;
+
+clients = []
+
+io.on('connection', socket => {
+
+    var localUsername = 'Unknown'
+
+    socket.emit('welcome', 'Welcome to the chat !')
+    io.emit('newUser', 'A user has entered the chat')
+    
+    socket.on('message', (data) => {
+        try {
+            let username = data.username
+            localUsername = username
+
+            if (!clients.includes(username)) clients.push(username)
+            let message = data.message
+            io.emit('message', {username, message})
+        } catch (error) {
+            console.log('An error occured upon message receival : ' + error)
+        }
+    })   
+    
+    socket.on('disconnect', () => {
+        io.emit('userLeft', {username: localUsername})
+    })
+})
+
+http.listen(SERVER_PORT, () => {
+    console.log(`Socket Server on Port: ${SERVER_PORT}`)
+})
